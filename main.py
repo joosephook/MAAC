38e4fa413bb23d62edd637ea05f0ea3b35f8da96
Joosep Hook <joosephook@gmail.com>
2019-09-04 11:35:06 +0100

    added total and mean episode rewards for easy overview

--- main.py
+++ main.py
@@ -155,6 +155,9 @@ def run(config):
             for a_i, a_ep_rew in enumerate(ep_rews):
                 logger.add_scalar('agent%i/mean_episode_rewards' % a_i, a_ep_rew, ep_i)
 
+            logger.add_scalar('total_episode_rewards', sum(ep_rews), ep_i)
+            logger.add_scalar('total_mean_episode_rewards', sum(ep_rews), ep_i)
+
             if ep_i % config.save_interval < config.n_rollout_threads:
                 model.prep_rollouts(device='cpu')
                 os.makedirs(run_dir / 'incremental', exist_ok=True)
